359035dffca1d5ac96e195f0921698e984343262
ZIYAN211 <nnm22cs211@nmamit.in>
2026-03-28 21:16:15 +0530

    update logo

--- vite.config.ts
+++ vite.config.ts
@@ -12,26 +12,24 @@ export default defineConfig(({mode}) => {
       tailwindcss(),
       VitePWA({
         registerType: 'autoUpdate',
-        includeAssets: ['envoilogo.png', 'ezyinvoice.png', 'logo512.png'],
+        includeAssets: ['envoilogo.png', 'ezyinvoice.png'],
         manifest: {
           short_name: "EzyInvoice",
           name: "EzyInvoice Offline Ledger",
           icons: [
-            {
-              src: "envoilogo.png",
-              sizes: "64x64 32x32 24x24 16x16",
-              type: "image/x-icon"
-            },
-            {
-              src: "logo192.png",
-              type: "image/png",
-              sizes: "192x192"
-            },
-            {
-              src: "logo512.png",
-              type: "image/png",
-              sizes: "512x512"
-            }
+      {
+        src: "envoilogo.png",
+        type: "image/png",
+        sizes: "192x192",
+        purpose: "any maskable"
+      },
+      {
+        src: "envoilogo.png",
+        type: "image/png",
+        sizes: "512x512",
+        purpose: "any maskable"
+      }
+          
           ],
           start_url: ".",
           display: "standalone",
